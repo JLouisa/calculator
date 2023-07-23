6eeb4cc578ab75882f1c8e41685846fcdd8bbca4
Jonathan Louisa <jonathan.louisa@gmail.com>
2023-07-23 12:10:15 -0400

    Create function to take 2 numbers and an operator

--- javascript.js
+++ javascript.js
@@ -13,3 +13,13 @@ function calcDivide(num1, num2) {return num1 / num2};
 let getNum1;
 let getNum2;
 let theOperator;
+
+function operate(n1, op, n2) {
+    if(op == '+') {return calcAdd(n1,n2)};
+    if(op == '-') {return calcSub(n1,n2)};
+    if(op == '*') {return calcMulti(n1,n2)};
+    if(op == '/') {return calcDivide(n1,n2)};
+    return 'Error';
+}
+
+console.log(operate(98, '*', 100));
