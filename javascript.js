70822dbd323b2b1856c8cffbf5209a4a28a86c99
Jonathan Louisa <jonathan.louisa@gmail.com>
2023-07-23 12:04:51 -0400

    Add add, sub, div and multi functions

---
+++ javascript.js
@@ -0,0 +1,11 @@
+// Calculate additions
+function calcAdd(num1, num2) {return num1 + num2};
+
+// Calculate Subtraction
+function calcSub(num1, num2) {return num1 - num2};
+
+// Calculate Multiplication
+function calcMulti(num1, num2) {return num1 * num2};
+
+// Calculate Division
+function calcDivide(num1, num2) {return num1 / num2};
